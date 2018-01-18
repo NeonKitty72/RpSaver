12805fff09b8bb3b19b2ebf57d7a30479c613356
Suzy Kersten <suzykersten72@outlook.com>
2018-01-18 13:33:14 -0600

    notes on making a universal generator class
    Basically how to make a go-between between the input RP in different styles and my webpage output.

---
+++ js/RPGenerator.js
@@ -0,0 +1,19 @@
+/****************************************************************
+*   Suzanne Kersten
+*	1/17/2017
+*	Generates RP output based on XML-like input.
+*
+*   TODO: 
+*****************************************************************/
+
+class Metasymbols{
+	constructor(symbol, isWrappedMetasymbol, beginningTag, endingTag){
+		this.symbol = symbol;
+        this.isWrappedMetasymbol = isWrappedMetasymbol;
+        this.beginningTag = beginningTag;
+        this.endingTag = endingTag;
+
+	}
+    
+    
+}
